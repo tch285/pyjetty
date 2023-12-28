e1f9d20c55185a715e4b0e40cecbe9bec5d365a2
Tucker Hwang <tucker_hwang@berkeley.edu>
2023-12-27 21:26:28 -0800

    Refactor processing code for clarity, sorting by ENC type

--- pyjetty/alice_analysis/process/user/thwang/process_ceecs_root.py
+++ pyjetty/alice_analysis/process/user/thwang/process_ceecs_root.py
@@ -24,6 +24,9 @@ import ecorrel
 
 from pyjetty.alice_analysis.process.base import process_base
 
+import cEEC_utils.gen_utils as gutils
+
+
 # Prevent ROOT from stealing focus when plotting
 ROOT.gROOT.SetBatch(True)
 # Automatically set Sumw2 when creating new histograms
@@ -47,7 +50,6 @@ class PythiaGenENC(process_base.ProcessBase):
     # Constructor
     #---------------------------------------------------------------
     def __init__(self, input_file='', config_file='', output_dir='', debug_level=0, args=None, **kwargs):
-
         super(PythiaGenENC, self).__init__(
             input_file, config_file, output_dir, debug_level, **kwargs)
 
@@ -81,9 +83,7 @@ class PythiaGenENC(process_base.ProcessBase):
         self.dphi_cut = -9999
         self.deta_cut = -9999
         self.npoint = config["npoint"]
-        self.npower = config["npower"]
-
-        
+        self.npower = config["npower"] 
 
     #---------------------------------------------------------------
     # Main processing function
@@ -122,59 +122,31 @@ class PythiaGenENC(process_base.ProcessBase):
     # Initialize histograms
     #---------------------------------------------------------------
     def initialize_hist(self):
-        pt_bins = linbins(self.pT_min, self.pT_max, self.pT_nbins)
-        RL_bins = logbins(self.RL_min, self.RL_max, self.RL_nbins)
-        Nconst_bins = linbins(self.Nconst_min, self.Nconst_max, self.Nconst_nbins)
+        pt_bins, _ = gutils.linbins(self.pT_min, self.pT_max, self.pT_nbins)
+        RL_bins, _, _, _, _ = gutils.logbins(self.RL_min, self.RL_max, self.RL_nbins)
+        Nconst_bins, _ = gutils.linbins(self.Nconst_min, self.Nconst_max, self.Nconst_nbins)
         self.hNevents = ROOT.TH1I("hNevents", 'Number accepted events (unscaled)', 2, -0.5, 1.5)
-        # setting these as TH1D not TH1I since I will need to scale them later
-        self.jetpT_ch = ROOT.TH1D("h_jetpT_ch", 'jet p_{T} distribution;jet p_{T} (GeV);Counts', self.pT_nbins, pt_bins)
-        self.jetpT_h = ROOT.TH1D("h_jetpT_h", 'jet p_{T} distribution;jet p_{T} (GeV);Counts', self.pT_nbins, pt_bins)
 
         # dictionary for storing histograms
         self.hists = {}
-        # enc/nconst/jetpt, jetR, jet level, npoint, enctype
 
-        def add_ENC_histogram(name):
-            print(f"Initializing histogram {name}")
-            h = ROOT.TH2D(name, name, self.pT_nbins, pt_bins, self.RL_nbins, RL_bins)
-            setattr(self, name, h)
-            getattr(self, hist_list_name).append(h)
+        self.hists['ENC'] = {}
+        self.hists['jetpT'] = {}
+        self.hists['Nconst'] = {}
 
-        for jetR in self.jetR_list:
-            # Store a list of all the histograms just so that we can rescale them later
-            R_label = str(jetR).replace('.', '') + 'Scaled'
-            hist_list_name = "hist_list_R%s" % str(jetR).replace('.', '')
-            setattr(self, hist_list_name, [])
-
-
-            for jet_level in self.jet_levels:
-                # ENC histograms (jet level == part level)
-                for ipoint in range(2, self.npoint+1):
-                    add_ENC_histogram(f'h_ENC{ipoint}TR_JetPt_{jet_level}_R{R_label}_trk00')
-                    add_ENC_histogram(f'h_ENC{ipoint}TR_JetPt_{jet_level}_R{R_label}_trk10')
-                    add_ENC_histogram(f'h_ENC{ipoint}QQ_JetPt_{jet_level}_R{R_label}_trk10')
-                    add_ENC_histogram(f'h_ENC{ipoint}PP_JetPt_{jet_level}_R{R_label}_trk10')
-                    add_ENC_histogram(f'h_ENC{ipoint}PM_JetPt_{jet_level}_R{R_label}_trk10')
-                    add_ENC_histogram(f'h_ENC{ipoint}MM_JetPt_{jet_level}_R{R_label}_trk10')
-                    # add_ENC_histogram(f'h_ENC{ipoint}AL_JetPt_{jet_level}_R{R_label}_trk10')
-
-                    # only save charge separation for pT>1GeV for now
-                    # if jet_level == "ch":
-                    #     add_ENC_histogram(f'h_ENC{ipoint}_JetPt_{jet_level}_R{R_label}_unlike_trk10')
-                    #     add_ENC_histogram(f'h_ENC{ipoint}_JetPt_{jet_level}_R{R_label}_like_trk10')
-
-                # Jet pt vs N constituents
-                name = f'h_Nconst_JetPt_{jet_level}_R{R_label}_trk00'
-                print('Initialize histogram', name)
-                h = ROOT.TH2D(name, f"{name};jet pT (GeV);Nconst", self.pT_nbins, pt_bins, self.Nconst_nbins, Nconst_bins)
-                setattr(self, name, h)
-                getattr(self, hist_list_name).append(h)
-
-                name = f'h_Nconst_JetPt_{jet_level}_R{R_label}_trk10'
-                print('Initialize histogram', name)
-                h = ROOT.TH2D(name, f"{name};jet pT (GeV);Nconst", self.pT_nbins, pt_bins, self.Nconst_nbins, Nconst_bins)
-                setattr(self, name, h)
-                getattr(self, hist_list_name).append(h)
+        for jet_level in self.jet_levels:
+            self.hists['ENC'][jet_level] = {}
+            self.hists['jetpT'][jet_level] = {}
+            self.hists['Nconst'][jet_level] = {}
+            for jetR in self.jetR_list:
+                R_label = str(jetR).replace('.', '') + '_sc'
+                self.hists['jetpT'][jet_level][jetR] = ROOT.TH1D(f"h_jetpT_{jet_level}_R{R_label}", 'jet p_{T} distribution;jet p_{T} (GeV);Counts', self.pT_nbins, pt_bins)
+                self.hists['Nconst'][jet_level][jetR] = ROOT.TH2D(f'h_Nconst_jetpT_{jet_level}_R{R_label}_1', f"Jet constituents;jet pT (GeV);Nconst", self.pT_nbins, pt_bins, self.Nconst_nbins, Nconst_bins)
+                self.hists['ENC'][jet_level][jetR] = {}
+                for cEEC_type in gutils.cEEC_types:
+                    self.hists['ENC'][jet_level][jetR][cEEC_type] = {}
+                    for ipoint in range(2, self.npoint + 1):
+                        self.hists['ENC'][jet_level][jetR][cEEC_type][ipoint] = ROOT.TH2D(f'h_ENC{ipoint}{cEEC_type}_jetpT_{jet_level}_R{R_label}_1', f'ENC{ipoint}{cEEC_type};jet p_{{T}} (GeV);R_{{L}}', self.pT_nbins, pt_bins, self.RL_nbins, RL_bins)
 
     #---------------------------------------------------------------
     # Initiate jet defs, selectors, and sd (if required)
@@ -183,26 +155,18 @@ class PythiaGenENC(process_base.ProcessBase):
         self.jet_def = {}
         self.jet_selector = {}
         for jetR in self.jetR_list:
-            # jetR_str = str(jetR).replace('.', '')
             # set up our jet definition and a jet selector
             jet_def = fj.JetDefinition(fj.antikt_algorithm, jetR)
             self.jet_def[jetR] = jet_def
-            # setattr(self, f"jet_def_R{jetR_str}", jet_def)
 
-            jet_selector = fj.SelectorPtMin(self.min_jet_pT) & fj.SelectorAbsEtaMax(self.max_eta_hadron - jetR) # FIX ME: use 5 or lower? use it on all ch, h, p jets?
+            jet_selector = fj.SelectorPtMin(self.min_jet_pT) & fj.SelectorAbsEtaMax(self.max_eta_hadron - jetR) # use 5 or lower? use it on all ch, h, p jets?
             self.jet_selector[jetR] = jet_selector
-            # setattr(self, f"jet_selector_R{jetR_str}", jet_selector)
             print(jet_def)
 
         # pwarning('max eta for particles after hadronization set to', self.max_eta_hadron)
-        track_selector_ch = fj.SelectorPtMin(self.min_det_trk_pT)
-        setattr(self, "track_selector_ch", track_selector_ch)
+        self.det_track_selector = fj.SelectorPtMin(self.min_det_trk_pT)
 
-        # pfc_selector1 = fj.SelectorPtMin(1.)/
-        pfc_selector1 = fj.SelectorPtMin(self.min_jet_trk_pT)
-        setattr(self, "pfc_def_10", pfc_selector1)
-
-            
+        self.jet_track_selector = fj.SelectorPtMin(self.min_jet_trk_pT)
 
     #---------------------------------------------------------------
     # Calculate events and pass information on to jet finding
@@ -242,60 +206,53 @@ class PythiaGenENC(process_base.ProcessBase):
     def find_jets_fill_trees(self):
         # Loop over jet radii
         for jetR in self.jetR_list:
-            jetR_str = str(jetR).replace('.', '')
-            # jet_selector = getattr(self, f"jet_selector_R{jetR_str}")
-            # jet_def = getattr(self, f"jet_def_R{jetR_str}")
             jet_selector = self.jet_selector[jetR]
             jet_def = self.jet_def[jetR]
-            track_selector_ch = getattr(self, "track_selector_ch")
 
             jets_p = fj.sorted_by_pt(jet_selector(jet_def(self.parts_pythia_p)))
             jets_h = fj.sorted_by_pt(jet_selector(jet_def(self.parts_pythia_h)))
-            jets_ch = fj.sorted_by_pt(jet_selector(jet_def(track_selector_ch(self.parts_pythia_ch))))
+            jets_ch = fj.sorted_by_pt(jet_selector(jet_def(self.det_track_selector(self.parts_pythia_ch))))
 
             for jet_level in self.jet_levels:
                 # Get the jets at different levels
                 if jet_level == "p":
                     jets = jets_p
-                if jet_level == "h":
+                elif jet_level == "h":
                     jets = jets_h
-                    target_jetpT_hist = self.jetpT_h
-                if jet_level == "ch":
+                    target_jetpT_hist = self.hists['jetpT'][jet_level][jetR]
+                elif jet_level == "ch":
                     jets = jets_ch
-                    target_jetpT_hist = self.jetpT_ch
+                    target_jetpT_hist = self.hists['jetpT'][jet_level][jetR]
 
                 #-------------------------------------------------------------
                 # loop over jets and fill EEC histograms with jet constituents
                 for j in jets:
                     target_jetpT_hist.Fill(j.perp())
-                    self.fill_jet_histograms(jet_level, j, f"{jetR_str}Scaled")
+                    self.fill_jet_histograms(jet_level, j, jetR)
 
     #---------------------------------------------------------------
     # Form EEC using jet constituents
     #---------------------------------------------------------------
-    def fill_jet_histograms(self, level, jet, R_label):
-        # leading track selection
+    def fill_jet_histograms(self, jet_level, jet, jetR):
+        # ignore jet if constituents are all below cutoff
         if self.leading_jet_const_pT > 0:
             constituents = fj.sorted_by_pt(jet.constituents())
             if constituents[0].perp() < self.leading_jet_const_pT:
                 return
 
-        # fill EEC histograms for jet constituents
-        pfc_selector1 = getattr(self, "pfc_def_10")
-
         # select all constituents with no cut
-        _c_select0 = fj.vectorPJ()
-        for c in jet.constituents():
-            if self.do_theory_check:
-                if pythiafjext.getPythia8Particle(c).charge()!=0:
-                    _c_select0.push_back(c)
-            else:
-                _c_select0.push_back(c)
-        cb0 = ecorrel.CorrelatorBuilder(_c_select0, jet.perp(), self.npoint, self.npower, self.dphi_cut, self.deta_cut)
+        # _c_select0 = fj.vectorPJ()
+        # for c in jet.constituents():
+        #     if self.do_theory_check:
+        #         if pythiafjext.getPythia8Particle(c).charge()!=0:
+        #             _c_select0.push_back(c)
+        #     else:
+        #         _c_select0.push_back(c)
+        # cb0 = ecorrel.CorrelatorBuilder(_c_select0, jet.perp(), self.npoint, self.npower, self.dphi_cut, self.deta_cut)
 
         # select constituents with 1 GeV cut
         _c_select1 = fj.vectorPJ()
-        for c in pfc_selector1(jet.constituents()):
+        for c in self.jet_track_selector(jet.constituents()):
             if self.do_theory_check:
                 if pythiafjext.getPythia8Particle(c).charge()!=0:
                     _c_select1.push_back(c)
@@ -304,13 +261,13 @@ class PythiaGenENC(process_base.ProcessBase):
         cb1 = ecorrel.CorrelatorBuilder(_c_select1, jet.perp(), self.npoint, self.npower, self.dphi_cut, self.deta_cut)
 
         for ipoint in range(2, self.npoint+1):
-            for index in range(cb0.correlator(ipoint).rs().size()):
-                getattr(self, f'h_ENC{ipoint}TR_JetPt_{level}_R{R_label}_trk00').Fill(jet.perp(), cb0.correlator(ipoint).rs()[index], cb0.correlator(ipoint).weights()[index])
+            # for index in range(cb0.correlator(ipoint).rs().size()):
+                # getattr(self, f'h_ENC{ipoint}TR_JetPt_{level}_R{R_label}_trk00').Fill(jet.perp(), cb0.correlator(ipoint).rs()[index], cb0.correlator(ipoint).weights()[index])
             for index in range(cb1.correlator(ipoint).rs().size()):
-                getattr(self, f'h_ENC{ipoint}TR_JetPt_{level}_R{R_label}_trk10').Fill(jet.perp(), cb1.correlator(ipoint).rs()[index], cb1.correlator(ipoint).weights()[index])
+                self.hists['ENC'][jet_level][jetR]['TR'][ipoint].Fill(jet.perp(), cb1.correlator(ipoint).rs()[index], cb1.correlator(ipoint).weights()[index])
             
         # if analyzing charged jet, separate different charge combinations
-        if level == "ch":
+        if jet_level == "ch":
             for ipoint in range(2, self.npoint+1):
                 # only fill trk pt > 1 GeV here for now
                 for index in range(cb1.correlator(ipoint).rs().size()):
@@ -318,24 +275,16 @@ class PythiaGenENC(process_base.ProcessBase):
                     part2 = int(cb1.correlator(ipoint).indices2()[index])
                     c1 = pythiafjext.getPythia8Particle(_c_select1[part1]).charge()
                     c2 = pythiafjext.getPythia8Particle(_c_select1[part2]).charge()
-                    if c1 > 0 and c2 > 0:
-                        getattr(self, f'h_ENC{ipoint}PP_JetPt_{level}_R{R_label}_trk10').Fill(jet.perp(), cb1.correlator(ipoint).rs()[index], cb1.correlator(ipoint).weights()[index])
-                    elif c1 < 0 and c2 < 0:
-                        getattr(self, f'h_ENC{ipoint}MM_JetPt_{level}_R{R_label}_trk10').Fill(jet.perp(), cb1.correlator(ipoint).rs()[index], cb1.correlator(ipoint).weights()[index])
+                    if c1 > 0 and c2 > 0: # both positively charged
+                        self.hists['ENC'][jet_level][jetR]['PP'][ipoint].Fill(jet.perp(), cb1.correlator(ipoint).rs()[index], cb1.correlator(ipoint).weights()[index])
+                    elif c1 < 0 and c2 < 0: # both negatively charged
+                        self.hists['ENC'][jet_level][jetR]['MM'][ipoint].Fill(jet.perp(), cb1.correlator(ipoint).rs()[index], cb1.correlator(ipoint).weights()[index])
                     else: # one is positive, one is negative
-                        getattr(self, f'h_ENC{ipoint}PM_JetPt_{level}_R{R_label}_trk10').Fill(jet.perp(), cb1.correlator(ipoint).rs()[index], cb1.correlator(ipoint).weights()[index])
+                        self.hists['ENC'][jet_level][jetR]['PM'][ipoint].Fill(jet.perp(), cb1.correlator(ipoint).rs()[index], cb1.correlator(ipoint).weights()[index])
                     
-                    getattr(self, f'h_ENC{ipoint}QQ_JetPt_{level}_R{R_label}_trk10').Fill(jet.perp(), cb1.correlator(ipoint).rs()[index], c1 * c2 * cb1.correlator(ipoint).weights()[index])
+                    self.hists['ENC'][jet_level][jetR]['QQ'][ipoint].Fill(jet.perp(), cb1.correlator(ipoint).rs()[index], c1 * c2 * cb1.correlator(ipoint).weights()[index])
 
-                    # if pythiafjext.getPythia8Particle(c1).charge()*pythiafjext.getPythia8Particle(c2).charge() < 0:
-                    #     # print("unlike-sign pair ",pythiafjext.getPythia8Particle(c1).id(),pythiafjext.getPythia8Particle(c2).id())
-                    #     getattr(self, f'h_ENC{ipoint}_JetPt_{level}_R{R_label}_unlike_trk10').Fill(jet.perp(), cb1.correlator(ipoint).rs()[index], cb1.correlator(ipoint).weights()[index])
-                    # else:
-                    #     # print("likesign pair ",pythiafjext.getPythia8Particle(c1).id(),pythiafjext.getPythia8Particle(c2).id())
-                    #     getattr(self, f'h_ENC{ipoint}_JetPt_{level}_R{R_label}_like_trk10').Fill(jet.perp(), cb1.correlator(ipoint).rs()[index], cb1.correlator(ipoint).weights()[index])
-
-        getattr(self, f'h_Nconst_JetPt_{level}_R{R_label}_trk00').Fill(jet.perp(), len(_c_select0))
-        getattr(self, f'h_Nconst_JetPt_{level}_R{R_label}_trk10').Fill(jet.perp(), len(_c_select1))
+        self.hists['Nconst'][jet_level][jetR].Fill(jet.perp(), len(_c_select1))
        
     #---------------------------------------------------------------
     # Initiate scaling of all histograms and print final simulation info
@@ -347,13 +296,19 @@ class PythiaGenENC(process_base.ProcessBase):
         with open(f"{self.output_dir}/scales.txt", 'w') as f:
             f.write(f"{scale_f}")
 
-        for jetR in self.jetR_list:
-            hist_list_name = f"hist_list_R{str(jetR).replace('.', '')}"
-            for h in getattr(self, hist_list_name):
-                h.Scale(scale_f)
+        self._scale_hists(self.hists['ENC'], scale_f)
+        self._scale_hists(self.hists['Nconst'], scale_f)
 
         print(f"N total final events:{self.hNevents.GetBinContent(1)} with {pythia.info.nAccepted() - self.hNevents.GetBinContent(1)} events rejected at hadronization step")
         self.hNevents.SetBinError(1, 0)
+    
+    def _scale_hists(self, value, scale):
+        # need this funky way to get the ROOT histograms - iterate through dictionary values until you find a non-dictionary
+        if not isinstance(value, dict):
+            value.Scale(scale)
+        else:
+            for val in value.values():
+                self._scale_hists(val, scale)
 
 ################################################################
 if __name__ == '__main__':
