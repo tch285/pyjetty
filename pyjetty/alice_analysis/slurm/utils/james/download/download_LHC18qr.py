60eaca2301f26cece5c1e3ff8448b67b85b2cb6f
Tucker Hwang <tucker_hwang@berkeley.edu>
2025-10-16 04:30:40 -0700

    Add 18qr download config

--- pyjetty/alice_analysis/slurm/utils/james/download/download_LHC18qr.py
+++ pyjetty/alice_analysis/slurm/utils/james/download/download_LHC18qr.py
@@ -11,12 +11,19 @@ On hiccup:
 
 On perlmutter:
   - start screen session with `screen`
-  - enter shifter image with `shifter --image=docker:sweisz/alice-grid-strace:1.0 --module=cvmfs`
-  - enter environment with `/cvmfs/alice.cern.ch/bin/alienv enter AliPhysics/vAN-20241023_O2-1`
+  - enter shifter image with 
+      shifter --image=docker:sweisz/alice-grid-strace:1.0 --module=cvmfs
+  - enter environment with 
+      /cvmfs/alice.cern.ch/bin/alienv enter AliPhysics/vAN-20250907_O2-1
     or the AliPhysics environment of your choice
-  - generate AliEn token with `alien-token-init`
-  - run script with `python download_data.py -c LHC20g4.yaml`
-  
+  - cd to this `download` directory
+  - enter the Python environment with
+      source .venv/bin/activate
+  - generate AliEn token with
+      alien-token-init
+  - run script with 
+      python3 download_LHC18qr.py -c LHC18q_pass3.yaml
+
 Note that if token expires or otherwise crashes, the script will automatically detect where to start copying again
 
 '''
@@ -27,6 +34,7 @@ import multiprocessing as mp
 import os
 import queue
 import subprocess
+import itertools as itools
 import sys
 import time
 import re
@@ -78,111 +86,69 @@ def download_data(config_file, log_level):
     if not args.config.is_file():
         logger.critical(f'File "{args.config}" does not exist; exiting.')
         sys.exit(404)
-    
+
     # Initialize config
     logger.info('Configuring...')
     logger.info(f'Configuration file set: "{args.config}"')
 
     with open(config_file, 'r') as stream:
       config = yaml.safe_load(stream)
-    
-    period = config['period']
-    parent_dir = config['parent_dir']
-    year = config['year']
-    train_name = config['train_name']
-    train_PWG = config['train_PWG']
-    train_tag = config['train_tag']
-    runlist = config['runlist']
-    output_dir = config['output_dir']
-    max_attempts = config['max_attempts'] if 'max_attempts' in config else 5
-    
-    if 'childno' in config:
-        childno = config['childno']
-    else:
-        childno = None
-    if 'recopass' in config:
-        recopass = config['recopass']
-    else:
-        recopass = None
-    if 'trigclus' in config:
-        trigclus = config['trigclus']
-    else:
-        trigclus = None
 
-    if 'pt_hat_bins' in config:
-        pt_hat_bins = config['pt_hat_bins']
-        n_pt_hat_bins = len(pt_hat_bins)
-    else:
-        pt_hat_bins = None
-        n_pt_hat_bins = None
+    runlist = config['params']['run']
+    output_dir = config['output_dir']
+    train_output = config['train_output']
+    max_attempts = config.get('max_attempts', 5)
+
+    params = config['params']
+    param_names = params.keys()
+    param_vals = params.values()
+    for pars in itools.product(*param_vals):
+        kwargs = {name: val for name, val in zip(param_names, pars)}
+        print(train_output.format(**kwargs))
 
     # Create output dir and cd into it
-    output_end = period if parent_dir == 'sim' else f"{period}_{trigclus}"
-    output_dir = os.path.join(output_dir, output_end)
     if not os.path.exists(output_dir):
-      os.makedirs(output_dir)
+        os.makedirs(output_dir)
     os.chdir(output_dir)
     logger.info(f'Output: {output_dir}')
 
     prog_queue = mp.Queue()
     nruns = len(runlist)
-    nloops = n_pt_hat_bins * nruns if parent_dir == 'sim' else 0
-    
+    nloops = nruns
+
     processes = []
     # Loop through runs, and start a download for each run in parallel
     logger.info(f"Downloading {nruns} runs.")
     start = time.time()
     for run_idx, run in enumerate(runlist):
-        p = mp.Process(target=download_run, args=(run_idx, prog_queue, parent_dir, year,
-                                                  period, run, train_PWG, train_name, train_tag,
-                                                  pt_hat_bins, childno, recopass, trigclus, max_attempts))
+        p = mp.Process(target=download_run_data, args=(run_idx, prog_queue, train_output.format(run = run), run, max_attempts))
         processes.append(p)
         p.start()
-    
+
     total_prog_bar = tqdm(total=nloops, unit='loop', desc='Overall', position=nruns)
 
-    if parent_dir == 'sim':
-        run_prog_bars = [tqdm(total=n_pt_hat_bins, unit='bin', desc=f"{i+1:02}: {run}", position=i) for i, run in enumerate(runlist)]
-        completed_runs = 0
-        while completed_runs < nruns:
-            try:
-                run_idx, increment = prog_queue.get(timeout=0.5)
-                if increment == "DONE":
-                    completed_runs += 1
-                    total_prog_bar.update(1)
-                elif isinstance(increment, tuple):
-                    _, nsubruns = increment
-                    run_prog_bars[run_idx].reset(total = nsubruns)
-                    total_prog_bar.total = total_prog_bar.total + nsubruns
-                    total_prog_bar.refresh()
-                else:
-                    total_prog_bar.update(increment)
-                    run_prog_bars[run_idx].update(increment)
-            except queue.Empty:
-                pass
-    elif parent_dir == 'data':
-        run_prog_bars = [tqdm(total=0, unit='subrun', desc=f"{i+1:>2d}: {run}", position=i) for i, run in enumerate(runlist)]
-        completed_runs = 0
-        while completed_runs < nruns:
-            try:
-                run_idx, increment = prog_queue.get(timeout=0.5)
-                if increment == "DONE":
-                    completed_runs += 1
-                    total_prog_bar.update(1)
-                elif isinstance(increment, tuple):
-                    _, nsubruns = increment
-                    run_prog_bars[run_idx].reset(total = nsubruns)
-                    total_prog_bar.total = total_prog_bar.total + nsubruns
-                    total_prog_bar.refresh()
-                else:
-                    total_prog_bar.update(increment)
-                    run_prog_bars[run_idx].update(increment)
-            except queue.Empty:
-                pass
+    run_prog_bars = [tqdm(total=0, unit='subrun', desc=f"{i+1:>2d}: {run}", position=i) for i, run in enumerate(runlist)]
+    completed_runs = 0
+    while completed_runs < nruns:
+        try:
+            run_idx, increment = prog_queue.get(timeout=0.5)
+            if increment == "DONE":
+                completed_runs += 1
+                total_prog_bar.update(1)
+            elif isinstance(increment, tuple):
+                _, nsubruns = increment
+                run_prog_bars[run_idx].reset(total = nsubruns)
+                total_prog_bar.total = total_prog_bar.total + nsubruns
+                total_prog_bar.refresh()
+            else:
+                total_prog_bar.update(increment)
+                run_prog_bars[run_idx].update(increment)
+        except queue.Empty:
+            pass
 
     for p in processes:
         p.join()
-    
+
     for bar in run_prog_bars:
         bar.close()
     total_prog_bar.close()
@@ -190,6 +156,15 @@ def download_data(config_file, log_level):
     logger.info(f"Download complete: {format_time_difference(time.time() - start)} elapsed.")
 
 #---------------------------------------------------------------------------
+def download_run_data(run_idx, prog_queue, train_output_dir, run, max_attempts):
+    logger = setup_process_logger(run_idx, run)
+    logger.info(f"Run {run}: starting download.")
+    download(train_output_dir, run, None, max_attempts, logger, prog_queue, run_idx)
+    # prog_queue.put((run_idx, 1))
+
+    prog_queue.put((run_idx, "DONE"))
+    logger.info(f"Run {run}: download complete.")
+
 def download_run(run_idx, prog_queue, parent_dir, year, period, run, train_PWG, train_name, train_tag, pt_hat_bins, childno, recopass, trigclus, max_attempts):
     logger = setup_process_logger(run_idx, run)
     logger.info(f"Run {run}: starting download.")
@@ -205,7 +180,7 @@ def download_run(run_idx, prog_queue, parent_dir, year, period, run, train_PWG, 
             download(train_output_dir, run, pt_hat_bin, max_attempts, logger, prog_queue, run_idx)
             logger.info(f"Bin {pt_hat_bin}: download complete.")
             prog_queue.put((run_idx, 1))
-    
+
     prog_queue.put((run_idx, "DONE"))
     logger.info(f"Run {run}: download complete.")
 
@@ -245,7 +220,7 @@ def download(train_output_dir, run, pt_hat_bin, max_attempts, logger, queue = No
         else:
             queue.put((run_idx, 1))
             continue
-        
+
         for i in range(max_attempts):
             cmd = f'alien_cp -f alien:{train_output_dir}/{subrun}/AnalysisResults.root file:{subrun_path}' # modified: file: prefix needed
             logger.info(f"Copy: {cmd}")
@@ -264,12 +239,12 @@ def download(train_output_dir, run, pt_hat_bin, max_attempts, logger, queue = No
         else:
             logger.error(f"Copy failed {max_attempts} times: {cmd}")
         queue.put((run_idx, 1))
-        
+
 
 def format_time_difference(time_diff):
     hours, remainder = divmod(time_diff, 3600)
     minutes, seconds = divmod(remainder, 60)
-    
+
     if hours > 0:
         return f"{int(hours)}h {int(minutes)}m {seconds:.2f}s"
     elif minutes > 0:
